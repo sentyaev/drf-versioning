b05f5ef4004ad32711b15ad9f889a4b5b4adb5a1
Vadim Sentyaev <sentyaev@gmail.com>
2020-03-18 12:48:35 +0100

    rename to drf_versioning

--- drf_versioning/wsgi.py
+++ drf_versioning/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rest1 project.
+WSGI config for drf_versioning project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_versioning.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest1.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_versioning.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
